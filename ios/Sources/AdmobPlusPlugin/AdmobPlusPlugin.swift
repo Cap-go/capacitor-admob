a3c731c70085074d77e122f23aad00f964f8734e
Martin Donadieu <martindonadieu@gmail.com>
2025-10-28 00:24:28 +0000

    refactor: remove unused echo method and related implementation

--- ios/Sources/AdmobPlusPlugin/AdmobPlusPlugin.swift
+++ ios/Sources/AdmobPlusPlugin/AdmobPlusPlugin.swift
@@ -7,17 +7,8 @@ public class AdmobPlusPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "AdmobPlusPlugin"
     public let jsName = "AdMobPlus"
     public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise),
         CAPPluginMethod(name: "getPluginVersion", returnType: CAPPluginReturnPromise)
     ]
-    private let implementation = AdmobPlus()
-
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
-    }
 
     @objc func getPluginVersion(_ call: CAPPluginCall) {
         call.resolve(["version": self.PLUGIN_VERSION])
