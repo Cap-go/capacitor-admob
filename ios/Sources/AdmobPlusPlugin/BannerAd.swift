dff35260d18d9b1346ab44a0ea534ccd1cbf8a3c
Felipe Zuleta <fzuleta@gmail.com>
2026-03-27 06:16:18 -0500

    restored the self.plugin?.bridge?.webView

--- ios/Sources/AdmobPlusPlugin/BannerAd.swift
+++ ios/Sources/AdmobPlusPlugin/BannerAd.swift
@@ -48,7 +48,8 @@ class BannerAd: NSObject, Ad {
                 return
             }
 
-            guard let viewController = self.plugin?.bridge?.viewController else {
+            guard let webView = self.plugin?.bridge?.webView,
+                  let viewController = self.plugin?.bridge?.viewController else {
                 completion(AdMobError.unknown("Unable to access view hierarchy"))
                 return
             }
